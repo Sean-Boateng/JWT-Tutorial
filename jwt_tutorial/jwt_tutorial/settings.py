11df0151b73bfd3c5f253d9b01eee34d45e23f56
Sean Boateng <seanboateng2@gmail.com>
2024-04-09 08:52:23 -0400

    cretaed registration serializer and view

--- jwt_tutorial/jwt_tutorial/settings.py
+++ jwt_tutorial/jwt_tutorial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication.app.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
 
 try:
     from jwt_tutorial.local_settings import *
