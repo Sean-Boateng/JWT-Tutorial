61fa02284db3c95c6a845ee9caa3bf691b766e90
Sean Boateng <seanboateng2@gmail.com>
2024-04-09 08:06:28 -0400

    FIxed Typo

--- jwt_tutorial/jwt_tutorial/asgi.py
+++ jwt_tutorial/jwt_tutorial/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for jwt_tutotial project.
+ASGI config for jwt_tutorial project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwt_tutotial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwt_tutorial.settings')
 
 application = get_asgi_application()
--- jwt_tutorial/jwt_tutorial/urls.py
+++ jwt_tutorial/jwt_tutorial/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for jwt_tutotial project.
+URL configuration for jwt_tutorial project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
